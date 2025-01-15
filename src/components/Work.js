8c0ab659b61677d13d862c83ffc324ed3369d5cb
saurabhkurve <saurabhkurve100@gmail.com>
2025-01-15 13:42:28 +0530

    new project added

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,7 +3,7 @@ import JobPortal from "../assets/projects/hireheaven.png";
 import Todo from "../assets/projects/todo.png";
 import Weather from "../assets/projects/weather.png";
 import Emoji from "../assets/projects/emoji.png";
-import RealEstate from "../assets/projects/realestate.jpg"
+import imageDb from "../assets/projects/imagedb.png"
 import ComingSoon from "../assets/projects/comingsoon.png"
 
 const Work = () => {
@@ -26,11 +26,11 @@ const Work = () => {
             <div className="opacity-0 group-hover:opacity-100">
               <span className="ml-5 text-2xl font-bold w-full   text-white tracking-wider">HireHeaven</span>
               <div className="pt-8 text-center">
-                <a href="https://hireheaven.netlify.app" target="_blank">
+                <a href="https://hireheaven.netlify.app" target="_blank"  rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
                 </a>
 
-                <a href="https://github.com/saurabhkurve/HireHeaven-A-Job-Portal" target="_blank">
+                <a href="https://github.com/saurabhkurve/HireHeaven-A-Job-Portal" target="_blank"  rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                 </a>
               </div>
@@ -42,11 +42,11 @@ const Work = () => {
             <div className="opacity-0 group-hover:opacity-100">
               <span className="ml-5 text-2xl font-bold w-full   text-white tracking-wider">Todo Application</span>
               <div className="pt-8 text-center">
-                <a href="https://todolistbysk.netlify.app/" target="_blank">
+                <a href="https://todolistbysk.netlify.app/" target="_blank"  rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
                 </a>
 
-                <a href="https://github.com/saurabhkurve/Todo-react-app" target="_blank">
+                <a href="https://github.com/saurabhkurve/Todo-react-app" target="_blank"  rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                 </a>
               </div>
@@ -58,11 +58,11 @@ const Work = () => {
             <div className="opacity-0 group-hover:opacity-100">
               <span className=" ml-5 text-2xl font-bold w-full   text-white tracking-wider">Weather Application</span>
               <div className="pt-8 text-center">
-                <a href="https://weather-app-tau-eight-86.vercel.app/" target="_blank">
+                <a href="https://weather-app-tau-eight-86.vercel.app/" target="_blank"  rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
                 </a>
 
-                <a href="https://github.com/saurabhkurve/Weather_App" target="_blank">
+                <a href="https://github.com/saurabhkurve/Weather_App" target="_blank"  rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                 </a>
               </div>
@@ -74,11 +74,27 @@ const Work = () => {
             <div className="opacity-0 group-hover:opacity-100">
               <span className="ml-5 text-2xl font-bold w-full   text-white tracking-wider">Emoji Game</span>
               <div className="pt-8 text-center">
-                <a href="https://emoji-game-pink.vercel.app/" target="_blank">
+                <a href="https://emoji-game-pink.vercel.app/" target="_blank"  rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
                 </a>
 
-                <a href="https://github.com/saurabhkurve/Emoji_Game" target="_blank">
+                <a href="https://github.com/saurabhkurve/Emoji_Game" target="_blank"  rel="noreferrer">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div style={{ backgroundImage: `url(${imageDb})` }} className="shadow-lg shadown-[#044c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            {/* Hover effect  */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="ml-10 text-2xl font-bold w-full   text-white tracking-wider">MovieDb</span>
+              <div className="pt-8 text-center">
+                <a href="https://movie-db-rho-navy.vercel.app/" target="_blank" rel="noreferrer">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
+                </a>
+
+                <a href="https://github.com/saurabhkurve/MovieDb" target="_blank" rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                 </a>
               </div>
@@ -101,21 +117,7 @@ const Work = () => {
             </div>
           </div>
 
-          <div style={{ backgroundImage: `url(${ComingSoon})` }} className="shadow-lg shadown-[#044c16] group container rounded-md flex justify-center items-center mx-auto content-div">
-            {/* Hover effect  */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold w-full   text-white tracking-wider">Coming Soon</span>
-              <div className="pt-8 text-center">
-                <a href="#">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
-                </a>
-
-                <a href="#">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
-                </a>
-              </div>
-            </div>
-          </div>
+          
 
           
         </div>
